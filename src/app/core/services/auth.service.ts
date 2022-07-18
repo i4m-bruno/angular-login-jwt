82f335f686efc380a05761950c7843047b3ca116
Bruno_Santos <ossbruno@outlook.com>
2022-07-17 22:11:15 -0300

    token jwt ++ guard ++ ✅

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError, map, Observable, throwError } from 'rxjs';
+import { JwtHelperService } from '@auth0/angular-jwt';
 // Services
 
 @Injectable({
@@ -13,8 +14,10 @@ export class AuthService {
   constructor(private http: HttpClient, private router: Router) {}
 
   public sign(payload: { email: string; password: string }): Observable<any> {
-    return this.http.post(`${this.url}/sign`, payload).pipe(
+    return this.http.post<{ token: string }>(`${this.url}/sign`, payload).pipe(
       map((res) => {
+        localStorage.removeItem('access_token');
+        localStorage.setItem('access_token', res.token);
         return this.router.navigate(['admin']);
       }),
       catchError((e) => {
@@ -26,5 +29,18 @@ export class AuthService {
         );
       })
     );
+  }
+
+  public logout() {
+    localStorage.removeItem('access_token');
+    return this.router.navigate(['']);
+  }
+
+  public isAuthenticated(): boolean {
+    const token = localStorage.getItem('access_token');
+    if (!token) return false;
+
+    const jwtHelper = new JwtHelperService();
+    return !jwtHelper.isTokenExpired(token);
   }
 }
